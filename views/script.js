d41c9b8184dcda15e0bcc0ea111ce1ebdc904063
netlan <netlan@yandex.ru>
2017-07-27 15:48:53 +0700

    Client-side error handling.

--- views/script.js
+++ views/script.js
@@ -5,26 +5,27 @@ function handleSubmit() {
   if (!document.forms.fileUpload.elements.userFile.value) return;
 
   if (document.forms.fileUpload.elements.userFile.files[0].size >= 5000000) {
-    document.getElementById("response").innerHTML = "LIMIT_FILE_SIZE is 5 Mb";
+    document.getElementById("response").innerHTML = "File size limit is 5 Mb";
     return;
   }
 
-  var xhr = new XMLHttpRequest(),
-      formData = new FormData(document.forms.fileUpload);
+  const xhr = new XMLHttpRequest(),
+        formData = new FormData(document.forms.fileUpload);
 
   xhr.open("POST", "/upload", true);
-
-  xhr.send(formData);
 
   xhr.onreadystatechange = function() {
     if (xhr.readyState !== 4) {
       return;
-    } else if (xhr.status !== 200) {
-      console.log(xhr.status + ": " + xhr.statusText);
-      document.getElementById("response").innerHTML = xhr.status;
+    }
+    if (xhr.status !== 200) {
+      document.getElementById("response").innerHTML = "Uploading error.";
     } else {
       document.forms.fileUpload.elements.userFile.value = "";
       document.getElementById("response").innerHTML = xhr.responseText;
     }
   };
+
+  xhr.send(formData);
+
 }
